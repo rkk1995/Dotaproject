274264a753be345177bf54f06c57d65e2a0af8f7
Ravindu <theravindu@gmail.com>
2019-06-23 09:50:26 -0400

    Initial

---
+++ playerid/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PlayeridConfig(AppConfig):
+    name = 'playerid'
---
+++ playerid/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+import requests
+import json
+def myView(request):
+     
+    return render(request, 'getplayerid.html')
+
+def playerData(request, player_id):
+    r = requests.get("https://api.opendota.com/api/players/" + str(player_id) +"/matches/?limit=20")
+    last20matches = json.loads(r.text)
+
+    return render (request, "playerstats.jinja", {'last20' : last20matches,'playerid' : player_id})
+
+def getplayerid(request):
+    content = request.POST.get('content')
+    return redirect('/players/{}/'.format(content))
